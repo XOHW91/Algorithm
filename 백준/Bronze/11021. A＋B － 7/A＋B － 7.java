1c781210a650168ef8b7ad13830dcc4072770a1d
XOHW91 <116329298+XOHW91@users.noreply.github.com>
2023-12-28 09:00:51 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 테스트 케이스 입력받음.
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        // 각 줄에 A와 B가 주어지며, Case #1 : 2 이렇게 합이 출력 됨.
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " "); // enter 기준으로 한 줄씩 받게되며, 공백 기준으로 문자를 자름.
+            bw.write("Case #" + (i+1) + ": " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        
+        // bw에 저장된 데이터 한꺼번에 출력함.
+        bw.flush();
+
+    }
+}
