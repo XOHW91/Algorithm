4e797f81fc414d15b1fd2ee47dff17e82c8f57f5
XOHW91 <so91062412@gmail.com>
2024-01-10 14:51:10 +0900

    [Bronze II] Title: 나머지, Time: 216 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,17 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 10개의 수를 입력받아 42로 나눈 나머지를 배열에 담는다.
-        Integer[] arr = new Integer[10];
-        int count = 0;
+        int[] arr = new int[10];
+
         for (int i = 0; i < 10; i++) {
-            int reminder = sc.nextInt() % 42;
-            if (!Arrays.asList(arr).contains(reminder)) {
-                arr[i] = reminder;
-                count++;
+            arr[i] = sc.nextInt() % 42;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j<arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    break;
+                }
             }
         }
-        
-        System.out.println(count);
-    }
+        System.out.println((arr.length - count));
+     }
 }
+
+
