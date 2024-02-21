d392aa39946a000f44d45d316880e8b2af92b4a7
XOHW91 <so91062412@gmail.com>
2024-02-21 11:49:11 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \. ". L_r'
+                   `~\/
+                      |
+                      |
+         */
+        String[] arr = new String[6];
+        arr[0] = "         ,r'\"7";
+        arr[1] = "r`-_   ,'  ,/";
+        arr[2] = " \\. \". L_r'";
+        arr[3] = "   `~\\/";
+        arr[4] = "      |";
+        arr[5] = "      |";
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+}
